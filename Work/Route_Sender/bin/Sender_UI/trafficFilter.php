6ad6a9af702bf985d7b7011eeb63e0cfd9abb0a5
waqarz <waqar.zia@gmail.com>
2015-09-18 17:52:11 +0200

    Essential part of last commit

---
+++ Work/Route_Sender/bin/Sender_UI/trafficFilter.php
@@ -0,0 +1,11 @@
+<?php
+
+ini_set('memory_limit','-1');//remove memory limit
+exec("sudo tc qdisc delete dev eth0 root");
+exec("sudo tc qdisc add dev eth0 root handle 1: cbq avpkt 1000 bandwidth 5000mbit");
+exec("sudo tc class add dev eth0 parent 1: classid 1:1 cbq rate 19000kbit allot 1500 prio 5 bounded isolated");
+exec("sudo tc filter add dev eth0 parent 1: protocol ip prio 16 u32 match ip dst 224.1.1.1 flowid 1:1");
+
+echo "Done setting traffic pattern!";
+
+?>
