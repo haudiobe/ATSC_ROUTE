65a5fe9e26743768d48c477b7f1f5b0a06602839
waqarz <waqar.zia@gmail.com>
2015-09-20 12:15:35 +0200

    Android specific changes for different directories.

--- Work/Route_Receiver/Receiver/ReceiverConfig/setIP.php
+++ Work/Route_Receiver/Receiver/ReceiverConfig/setIP.php
@@ -1,5 +1,5 @@
 <?php
-ini_set('memory_limit','-1');//remove memory limit
+
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -11,10 +11,14 @@ chdir("../../bin/");
 
 $ip=json_decode($_POST['ip']);
 echo $ip;
-$command= "sudo ../../Config/gensdp.sh ".$ip;
+$command= "sudo sh ../../Config/gensdp.sh ".$ip;
 $output=array();
 exec($command,$output);
 //var_dump($output);
+
+exec("sudo killall keepalive");
+$command= "sudo /data/local/tmp/keepalive ".$ip." &";
+exec($command);
 
 echo "done";
 ?>
